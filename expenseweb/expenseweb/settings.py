18f3ea4c4012fe4cafc58a6aaf2aa8e5cc1d2094
joel-varghese <joelvar17@outlook.com>
2026-09-12 19:26:52 -0400

    new changes to the app

--- expenseweb/expenseweb/settings.py
+++ expenseweb/expenseweb/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(Path(__file__).resolve().parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
